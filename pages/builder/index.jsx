a128111d163e0bfc4c17a2ed9cfab673aa789f4f
BlackKing1111 <drelord7411@gmail.com>
2026-02-11 06:55:43 -0600

    Create Builder component for bot building interface

---
+++ pages/builder/index.jsx
@@ -0,0 +1,8 @@
+export default function Builder() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>WolfieVerse Bot Builder</h1>
+      <p>Creators will build bots here.</p>
+    </div>
+  );
+}
