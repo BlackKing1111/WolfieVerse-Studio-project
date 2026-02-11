407572487f0456893da162584f4f4a417e331ac9
BlackKing1111 <drelord7411@gmail.com>
2026-02-11 06:41:00 -0600

    Add initial Marketplace component

---
+++ pages/marketplace/index.jsx
@@ -0,0 +1,8 @@
+export default function Marketplace() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Wolves' Den Marketplace</h1>
+      <p>This is the starting point. The 3D environment and wolves will be added next.</p>
+    </div>
+  );
+}
