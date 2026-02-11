8d13b9e4c7c9c6d28a2c729d976b73eb99503a6e
BlackKing1111 <drelord7411@gmail.com>
2026-02-11 07:56:46 -0600

    Create WolvesDen component for marketplace

---
+++ pages/marketplace/WolvesDen.jsx
@@ -0,0 +1,16 @@
+export default function WolvesDen() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        minHeight: "100vh",
+        backgroundColor: "#000",
+        color: "#fff",
+        padding: "40px",
+      }}
+    >
+      <h1>Wolves' Den Marketplace</h1>
+      <p>Visual components will appear here.</p>
+    </div>
+  );
+}
